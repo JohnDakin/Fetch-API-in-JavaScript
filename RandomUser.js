7ae3bc589f1e4ae3b1adcbb447ae65b8e4023862
John Dakin <jdakin749@gmail.com>
2026-08-04 14:15:45 +0300

    I've improved the random user fetch api project

--- RandomUser.js
+++ RandomUser.js
@@ -51,7 +51,7 @@ async function testRestApi(){
 
 testRestApi();
 */
-
+/*
 async function testRestApi(){
   let data = await fetch("https://api.restful-api.dev/objects/12", 
   {method: 'DELETE'});
@@ -62,3 +62,37 @@ async function testRestApi(){
 }
 
 testRestApi();
+
+*/
+
+const url = "https://randomuser.me/api/";
+
+
+async function getRandomUser(url){
+  try{
+    let response = await fetch(url);
+    // console.log(response);
+
+    if(!response.ok){
+      throw new Error("Unable to fetch resource, Response: ", response.status)
+    }
+
+    let data = await response.json()
+    console.log(`${data.results[0].name.first} ${data.results[0].name.last} is ${data.results[0].gender} in gender`);
+    let randomUserImage = data.results[0].picture.thumbnail;
+
+    let htmlImage = document.getElementById("randomUserImage");
+    let htmlP = document.getElementById("userDetail");
+
+    htmlImage.src =  randomUserImage;
+    htmlImage.style.display = "block";
+    htmlP.innerText  = `${data.results[0].name.first} ${data.results[0].name.last} is ${data.results[0].gender} in gender`;
+
+
+  }catch(error){
+    console.log("Error",error.message);
+  }
+  
+}
+
+getRandomUser(url);
